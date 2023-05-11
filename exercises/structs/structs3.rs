b54270e71dc3b79d9badaa36233f9f9e7d1f6947
Germán Andrés Delbianco <german@nomadic-labs.com>
2023-05-11 09:35:12 +0200

    nl2023: solve structs intro exos

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -26,12 +26,16 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.sender_country == self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i64 {
+        if cents_per_gram <= 0 {
+            panic!("I won't pay you to ship things")
+	} else {
+            i64::from(self.weight_in_grams) * i64::from(cents_per_gram)
+	}
     }
 }
 
